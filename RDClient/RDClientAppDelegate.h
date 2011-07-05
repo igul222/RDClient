0b9a96493233a45df3298da98ae517db5a35d487
Ishaan <igul222@gmail.com>
2011-07-05 13:05:25 -0400

    Initial Commit

---
+++ RDClient/RDClientAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RDClientAppDelegate.h
+//  RDClient
+//
+//  Created by Ishaan Gulrajani on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDClientAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ RDClient/iPhone/RDClientAppDelegate_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  RDClientAppDelegate_iPhone.h
+//  RDClient
+//
+//  Created by Ishaan Gulrajani on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RDClientAppDelegate.h"
+
+@interface RDClientAppDelegate_iPhone : RDClientAppDelegate
+
+@end
